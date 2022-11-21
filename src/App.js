f1be0518b824dfe015fc712c70600552796bc7cc
ozkankomu <ozkankomu35@gmail.com>
2022-11-21 13:31:10 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/people-task-cards/" element={<Home />} />
+        <Route path="/people-task-cards/:id" element={<Detail />} />
         <Route path="/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
