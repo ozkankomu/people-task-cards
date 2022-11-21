30a2feeaa7f92650a14282d8b16697f4bed82e1c
ozkankomu <ozkankomu35@gmail.com>
2022-11-21 13:18:24 +0300

    finished

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -25,7 +25,7 @@ const Modal = ({ form, setForm, todos, setTodos, id }) => {
       >
         <div className="modal-dialog">
           <div className="modal-content">
-            <div className="modal-header row justify-content-center">
+            <div className="modal-header row justify-content-center  border-0">
               <img
                 src={`https://i.pravatar.cc/500?img=${id}`}
                 className="card-img-top rounded-circle modalImage col-5 mb-2"
@@ -37,24 +37,24 @@ const Modal = ({ form, setForm, todos, setTodos, id }) => {
               </h1>
 
               <input
-                className="form-control  col-12"
+                className="form-control text-center col-12"
                 type="text"
                 value={form.title}
                 onChange={(e) => setForm({ ...form, title: e.target.value })}
               />
             </div>
 
-            <div className="modal-footer">
+            <div className="modal-footer ">
               <button
                 type="button"
-                className="btn btn-secondary"
+                className="btn btn-danger w-100"
                 data-bs-dismiss="modal"
               >
                 Close
               </button>
               <button
                 type="button"
-                className="btn btn-primary"
+                className="btn btn-primary w-100"
                 data-bs-dismiss="modal"
                 onClick={() => handleModal(form.id)}
               >
